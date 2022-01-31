646b6dc5743820b9a7b7e692f89cde63d40fae15
EamonnMul <eamonnmulhearne@hotmail.com>
2022-01-31 09:56:54 +0000

    Task 3

--- truffle-config.js
+++ truffle-config.js
@@ -51,6 +51,14 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       
+        gas: 4500000,        
+        confirmations: 2,
+        timeoutBlocks: 200,
+        skipDryRun: true,
+    },
 
     // Another network with more advanced options...
     // advanced: {
